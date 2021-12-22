17fbef42e60d09797cc02a613bf1d9d2367b9f41
danieldiv <das.29@outlook.com>
2021-12-22 14:21:22 -0300

    main modificado

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 int main(){
 
 	// int reg[] = {10,2,7,5,3,9,16,4,11,1,6,23,14};
-	int reg[] = {7,9,12,22,10,55,40,74,85,91,80,61,31,98, 1,33,99,54,13};
+	int reg[] = {7,9,12,22,10,61,74,40,31,85,91,80,98,55, 1,33,99,54,13};
 	int tam = sizeof(reg)/sizeof(reg[0]);
 	
 	int remove[] = {55,91,7,1,9,40,80};
@@ -17,14 +17,16 @@ int main(){
     	Insere(&btree, r);
   	}
 
+	printf("Arvore com todos os valores inseridors\n\n");
   	Imprime(&btree, 0);
 
 	for(int i=0; i<tamR; i++){
-		printf("\nremovendo o valor: %d\n", remove[i]);
+		printf("\nremovendo o valor: %d\n\n", remove[i]);
 		Retira(&btree, remove[i]);
 		Imprime(&btree, 0);
   	}
-	printf("\n");
+	
+	printf("\nArvore com os valores {55,91,7,1,9,40,80} removidos\n\n");
 	Imprime(&btree, 0);
 	
 	return 0;
